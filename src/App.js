adcb9db5703f1f1b3d270055f1d96f18974da22f
Vaibhav Upadhyay <96303359+vaibhavupadhyayy@users.noreply.github.com>
2023-12-09 17:10:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Editor from './component/Editor'
+
+function App() {
+  return (
+   <div className="app">
+    <Editor/>
+   </div>
+  );
+}
+
+export default App;
